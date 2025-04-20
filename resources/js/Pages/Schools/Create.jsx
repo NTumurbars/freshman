b8972d1d7cbc13ae06c4520b1e2c39c3d8ed24c4
ktsu2i <kaito.tsu2i@gmail.com>
2025-04-20 21:10:23 +0900

    change button label to Create on /schools/create

--- resources/js/Pages/Schools/Create.jsx
+++ resources/js/Pages/Schools/Create.jsx
@@ -71,7 +71,7 @@ export default function Create() {
                         type="submit"
                         className="rounded bg-blue-500 px-4 py-2 text-white hover:bg-blue-600"
                     >
-                        Save
+                        Create
                     </button>
                 </form>
             </div>
